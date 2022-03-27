8032013440a866da081ae18743f89c864f1b2e5a
Ihar Myshkevich <ih.myshkevich@gmail.com>
2022-03-27 18:24:39 +0200

    Actor CRUD

---
+++ app/Http/Controllers/ActorController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\actor;
+
+class ActorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $all = actor::all();
+        return $all;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $actor = new actor();
+        $actor->name = $request->name;
+        $actor->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $actor = actor::find($id);
+        return $actor;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $actor = actor::find($id);
+        $actor->name = $request->name;
+        $actor->save();
+        return $actor;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $actor = actor::find($id);
+        $actor->delete();
+    }
+}
